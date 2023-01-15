83a3c1c95458855297b4090ae740ba5b87ac12d6
pramodaya <pramodayajayalath@gmail.com>
2023-01-16 00:54:14 +0200

    Initial commit

---
+++ src/main/java/com/pramodaya/spring/boot/oauth2/SampleController.java
@@ -0,0 +1,15 @@
+package com.pramodaya.spring.boot.oauth2;
+
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class SampleController {
+    @GetMapping
+    public Object getCurrentUser(OAuth2AuthenticationToken oAuth2AuthenticationToken){
+        return oAuth2AuthenticationToken.getPrincipal().getAttributes();
+    }
+}
